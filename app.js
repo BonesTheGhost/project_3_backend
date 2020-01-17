7791f4b252fb98fa78108d626621dcb35e7e3727
BonesTheGhost <bonestheghost@gmail.com>
2020-01-16 20:19:43 -0600

    [1] UPGRADED ERROR HANDLING:: Streamlined the error handling process by manually returning a 404 if a route doesn't exist, made error messages more robust via '||' and planned ahead with next(error); for when we add a database later on with ASYNC code!

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const app = express();
 
 
 
-//Middleware
+// ===== ===== Middleware ===== =====
 //We want to route certain requests to certain funcitons.
 app.use('/api/scores', scoresRoutes);
+
+//ERROR HANDLING MIDDLEWARE FUNCTION:
+//It will execute if any middleware if front of it throws an error
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unknown error occured!'});
+});
+
+// ===== ===== ========== ===== =====
 
 
 app.listen(5000);
--- routes/scores-routes.js
+++ routes/scores-routes.js
@@ -27,11 +27,19 @@ const DUMMY_SCORES = [
 //router.use/get/post.get('path', (req, res, next) => {})
 router.get('/:sid', (req, res, next) => {
     const scoreId = req.params.sid;
+
     const score = DUMMY_SCORES.find(s => {
         return s.id === scoreId;
     });
-    console.log('GET request in Scores');
-    res.json({score: score}); // { score } => { score: score }
+
+    //For if we cannot find a score. 'return' makes it so that if this triggers, no other response is sent (which would cause an error).
+    if (!score) {
+        const error = new Error('Could not find a score for that id');
+        error.code = 404;
+        return next(error);
+    }
+    
+    res.json({ score: score }); // { score } => { score: score }
 });
 
 // '/api/scores/user/:uid
@@ -41,6 +49,13 @@ router.get('/user/:uid', (req, res, next) => {
     const score = DUMMY_SCORES.find(s => {
         return s.creator === userId;
     });
+
+    //Have to use next(error); with ASYNC code!!
+    if (!score) {
+        const error = new Error('Could not find a score for that USER id (creator)');
+        error.code = 404;
+        return next(error);
+    }
 
     res.json({ score: score });
 });

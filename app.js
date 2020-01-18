80284ee144cf63b5ed4fcf940d516ca509c063cc
BonesTheGhost <bonestheghost@gmail.com>
2020-01-17 22:38:19 -0600

    [?] Transfer machines.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const scoresRoutes = require('./routes/scores-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const scoresRoutes = require("./routes/scores-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
-const app = express(); 
-
-
+const app = express();
 
 // ===== ===== Middleware ===== =====
 //We want to route certain requests to certain funcitons.
@@ -15,25 +14,34 @@ const app = express();
 //This is necessary to grab data from the user and submit it to the backend.
 app.use(bodyParser.json());
 
-app.use('/api/scores', scoresRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/scores", scoresRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could note find this route.', 404);
-    throw error;
+  const error = new HttpError("Could note find this route.", 404);
+  throw error;
 });
 
 //ERROR HANDLING MIDDLEWARE FUNCTION:
 //It will execute if any middleware if front of it throws an error
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occured!'});
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
 });
 
 // ===== ===== ========== ===== =====
 
-//npm start
-app.listen(5000);
+//Connect to the db FIRST, since if this can't be completed, we don't need the server.
+//This connect returns a promise since connecitng to the server is an asynchronous task.
+mongoose
+  .connect()
+  .then(() => {
+    //npm start (Backend Server)
+    app.listen(5000);
+  })
+  .catch(err => {
+      console.log(err)
+  });
